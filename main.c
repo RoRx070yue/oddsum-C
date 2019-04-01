d9451d0ffd69f7be17c030d6013b1cb5fa9091b0
RoRx070yue <49121424+RoRx070yue@users.noreply.github.com>
2019-04-01 18:58:36 +0800

    Update main.c

--- main.c
+++ main.c
@@ -3,19 +3,18 @@
 
 int main()
 {
-int t, sum, a, b, i, loops;
+int case, sum, odd1, odd2, loops;
  scanf("%d", &t);
- for(i=1;i<=t;i++){
-    //printf("t=%d\n",i);
-  scanf("%d%d", &a, &b);
-  printf("Case %d: OddSum[%d..%d]", i, a, b);
+ for(int i=1;i<=case;i++){
+  scanf("%d%d", &odd1, &odd2);
+  printf("Case %d: OddSum[%d..%d]", i, odd1, odd2);
   a|=1;
-  for(sum=loops=0;a<=b;a+=2,loops++){
-      //printf("[loops %d:]",loops);
+  for(sum=loops=0;odd1<=odd2;odd1+=2,loops++){
+   //printf("[loops %d:]",loops);
    if(loops==0)printf("=");
    else printf("+");
-   printf("%d", a);
-   sum+=a;
+   printf("%d", odd1);
+   sum+=odd1;
   }
   if(loops==1) printf("\n");
   else printf("=%d\n", sum);
